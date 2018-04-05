310054034a291e044e6969281ff71ff1933ff7a9
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-05 15:47:16 -0600

    added next layer of html pages

--- app.js
+++ app.js
@@ -33,3 +33,66 @@ $(document).ready(function(){
 	return false;
 	});
 });
+
+$(document).ready(function(){
+	$("#option1.1").on("click", function() {
+	window.open("zoption1.1.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option1.2").on("click", function() {
+	window.open("zoption1.2.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option1.3").on("click", function() {
+	window.open("zoption1.3.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option2.1").on("click", function() {
+	window.open("zoption2.1.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option2.2").on("click", function() {
+	window.open("zoption2.2.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option2.3").on("click", function() {
+	window.open("zoption2.3.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option3.1").on("click", function() {
+	window.open("zoption3.1.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option3.2").on("click", function() {
+	window.open("zoption3.2.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option3.3").on("click", function() {
+	window.open("zoption3.3.html");
+	return false;
+	});
+});
