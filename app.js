120d30ae1b37090f004b7f281392f3d1bf355d1e
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-05 14:56:04 -0600

    another layer of pages added

--- app.js
+++ app.js
@@ -12,3 +12,24 @@ $(document).ready(function(){
 	return false;
 	});
 });
+
+$(document).ready(function(){
+	$("#option1").on("click", function() {
+	window.open("zoption1.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option2").on("click", function() {
+	window.open("zoption2.html");
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option3").on("click", function() {
+	window.open("zoption3.html");
+	return false;
+	});
+});
