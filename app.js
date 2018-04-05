bf6c0be9af8991ea4d455fda25a94bf14099fdff
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-05 13:41:32 -0600

    added links and additional html files

---
+++ app.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+
+$("#zombies").on("click", function() {
+	window.open("zombie.html");
+	return false;
+});
+
+$("#spacesuit").on("click", function() {
+	window.open("space.html");
+	return false;
+});
+
+ });
