c3d4476afce3dec95a88b9a374a016f1a23a8628
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-06 10:35:13 -0600

    updated file system

--- app.js
+++ app.js
@@ -1,98 +1,98 @@
 
 $(document).ready(function(){
 	$("#zombies").on("click", function() {
-	window.location.href="zombie.html";
+	window.location.href="zombieStory/zpage1/zombie.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#spacesuit").on("click", function() {
-	window.location.href="space.html";
+	window.location.href="spaceStory/spage1/space.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option1").on("click", function() {
-	window.location.href="zoption1.html";
+	window.location.href="zombieStory/zpage2/zoption1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2").on("click", function() {
-	window.location.href="zoption2.html";
+	window.location.href="zombieStory/zpage2/zoption2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option3").on("click", function() {
-	window.location.href="zoption3.html";
+	window.location.href="zombieStory/zpage2/zoption3.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option1-1").on("click", function() {
-	window.location.href="zoption1-1.html";
+	window.location.href="zombieStory/zpage3/zoption1-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option1-2").on("click", function() {
-	window.location.href="zoption1-2.html";
+	window.location.href="zombieStory/zpage3/zoption1-2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option1-3").on("click", function() {
-	window.location.href="zoption1-3.html";
+	window.location.href="zombieStory/zpage3/zoption1-3.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-1").on("click", function() {
-	window.location.href="zoption2-1.html";
+	window.location.href="zombieStory/zpage3/zoption2-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-2").on("click", function() {
-	window.location.href="zoption2-2.html";
+	window.location.href="zombieStory/zpage3/zoption2-2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-3").on("click", function() {
-	window.location.href="zoption2-3.html";
+	window.location.href="zombieStory/zpage3/zoption2-3.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option3-1").on("click", function() {
-	window.location.href="zoption3-1.html";
+	window.location.href="zombieStory/zpage3/zoption3-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option3-2").on("click", function() {
-	window.location.href="zoption3-2.html";
+	window.location.href="zombieStory/zpage3/zoption3-2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option3-3").on("click", function() {
-	window.location.href="zoption3-3.html";
+	window.location.href="zombieStory/zpage3/zoption3-3.html";
 	return false;
 	});
 });
