a4963b1b54c43a1e3d07873c19833c905a8a72dc
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-06 11:40:27 -0600

    worked on file system some more, got everything functional

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//starter
 $(document).ready(function(){
 	$("#zombies").on("click", function() {
 	window.location.href="zombieStory/zpage1/zombie.html";
@@ -9,90 +9,6 @@ $(document).ready(function(){
 $(document).ready(function(){
 	$("#spacesuit").on("click", function() {
 	window.location.href="spaceStory/spage1/space.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option1").on("click", function() {
-	window.location.href="zombieStory/zpage2/zoption1.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option2").on("click", function() {
-	window.location.href="zombieStory/zpage2/zoption2.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option3").on("click", function() {
-	window.location.href="zombieStory/zpage2/zoption3.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option1-1").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption1-1.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option1-2").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption1-2.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option1-3").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption1-3.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option2-1").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption2-1.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option2-2").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption2-2.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option2-3").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption2-3.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option3-1").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption3-1.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option3-2").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption3-2.html";
-	return false;
-	});
-});
-
-$(document).ready(function(){
-	$("#option3-3").on("click", function() {
-	window.location.href="zombieStory/zpage3/zoption3-3.html";
 	return false;
 	});
 });
---
+++ zombieStory/zpage1/app.js
@@ -0,0 +1,21 @@
+//page1
+$(document).ready(function(){
+	$("#option1").on("click", function() {
+	window.location.href="zpage2/zoption1.html";
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option2").on("click", function() {
+	window.location.href="zpage2/zoption2.html";
+	return false;
+	});
+});
+
+$(document).ready(function(){
+	$("#option3").on("click", function() {
+	window.location.href="zpage2/zoption3.html";
+	return false;
+	});
+});
