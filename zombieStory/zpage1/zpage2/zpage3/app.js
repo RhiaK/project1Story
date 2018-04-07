e4ad172caa3ad13cd087d93172214a734a6bb46b
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-07 17:29:27 -0600

    added content to all page 4s

--- zombieStory/zpage1/zpage2/zpage3/app.js
+++ zombieStory/zpage1/zpage2/zpage3/app.js
@@ -22,84 +22,84 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 	$("#option1-3-1").on("click", function() {
-	window.location.href="zpage3/zoption1-3-1.html";
+	window.location.href="zpage4/zoption1-3-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option1-3-2").on("click", function() {
-	window.location.href="zpage3/zoption1-3-2.html";
+	window.location.href="zpage4/zoption1-3-2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-1-1").on("click", function() {
-	window.location.href="zpage3/zoption2-1-1.html";
+	window.location.href="zpage4/zoption2-1-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-1-2").on("click", function() {
-	window.location.href="zpage3/zoption2-1-2.html";
+	window.location.href="zpage4/zoption2-1-2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-2-1").on("click", function() {
-	window.location.href="zpage3/zoption2-2-1.html";
+	window.location.href="zpage4/zoption2-2-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-2-2").on("click", function() {
-	window.location.href="zpage3/zoption2-2-2.html";
+	window.location.href="zpage4/zoption2-2-2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-3-1").on("click", function() {
-	window.location.href="zpage3/zoption2-3-1.html";
+	window.location.href="zpage4/zoption2-3-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option2-3-2").on("click", function() {
-	window.location.href="zpage3/zoption2-3-2.html";
+	window.location.href="zpage4/zoption2-3-2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option3-2-1").on("click", function() {
-	window.location.href="zpage3/zoption3-2-1.html";
+	window.location.href="zpage4/zoption3-2-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option3-2-2").on("click", function() {
-	window.location.href="zpage3/zoption3-2-2.html";
+	window.location.href="zpage4/zoption3-2-2.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option3-3-1").on("click", function() {
-	window.location.href="zpage3/zoption3-3-1.html";
+	window.location.href="zpage4/zoption3-3-1.html";
 	return false;
 	});
 });
 
 $(document).ready(function(){
 	$("#option3-3-2").on("click", function() {
-	window.location.href="zpage3/zoption3-3-2.html";
+	window.location.href="zpage4/zoption3-3-2.html";
 	return false;
 	});
 });
